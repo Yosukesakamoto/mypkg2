b294027e65f812d002704390716004b1992e5ffa
Yosuke Sakamoto <s21C1052MY@s.chibakoudai.jp>
2022-12-31 17:08:09 +0900

    Add a test

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -5,4 +5,14 @@ from std_msgs.msg import Int16
 rclpy.init()
 node = Node("talker")
 srv = node.create_service(Int16, "countup", 10)
-n = 0            
+n = 0
+
+def cb():
+    global n
+    msg = Int16()
+    msg.data = n
+    pub.publish(msg)
+    n += 1
+
+node.create_timer(0.5, cb)
+rclpy.spin(node)
