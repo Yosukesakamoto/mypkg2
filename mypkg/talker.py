a89927de36f218f1de83defdb2978a11cde0087e
Yosuke Sakamoto <s21C1052MY@s.chibakoudai.jp>
2022-12-31 16:00:18 +0900

    Add a test

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,22 +1,16 @@
 import rclpy                     
 from rclpy.node import Node      
-from std_msgs.msg import Int16   
-
-class Talker():
-    def __init__(self, node):
-        self.pub =  node.create_publisher(Int16, "countup", 10)
-        self.n = 0
-        node.create_timer(0.5, self.cb)
+from person_msgs.srv import Query
         
+def cb(request, response):
+    if request.name == "上田隆一":
+        response.age = 44
+    else:
+        response.age = 255
 
-    def cb(self):            
-        msg =Int16()
-        msg.data = self.n   
-        self.pub.publish(msg)        
-        self.n += 1
-
-
+    return response
+        
 rclpy.init()
 node = Node("talker")
-talker = Talker(node)
+srv = node.create_service(Query, "query", cb)
 rclpy.spin(node)            
