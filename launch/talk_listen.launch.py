e48170ef228d189540205194b2828e7fe2e2055f
Yosuke Sakamoto <s21C1052MY@s.chibakoudai.jp>
2023-01-07 01:13:29 +0900

    ADD

--- launch/talk_listen.launch.py
+++ launch/talk_listen.launch.py
@@ -1,8 +1,3 @@
-#!/usr/bin/python3
-# SPDX-FileCopyrightText: 2022 Yosuke Sakamoto　　　　
-# SPDX-License-Identifier: BSD-3-Clause
-
-
 import launch
 import launch.actions
 import launch.substitutions
